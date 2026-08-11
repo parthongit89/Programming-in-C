6f1828f6521606d56a7e891ab13a046a2df863de
Parth Sonavane <sonavaneparthgit@gmail.com>
2026-08-11 23:50:55 +0530

    Add files via upload

---
+++ Datatypes_and_variable.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    int a = 5 ; 
+    float b = 3.14; 
+    char c = 'p';
+    return  0;
+}
---
+++ Output.c
@@ -0,0 +1,9 @@
+// Output in C 
+// gcc Output.c; .\a.exe
+
+#include<stdio.h>
+int main(){
+    printf("This is output of printf\n");
+    printf("New line\n");
+    return 0 ;
+}
