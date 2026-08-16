b1b585ddcc1b89a8802df28a807de0b4ccbc98f6
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-16 12:52:07 +0530

    Problem_01 of function in C

---
+++ Chapter_05/problem_01.c
@@ -0,0 +1,21 @@
+// Problem_01
+# include <stdio.h>
+void printhello();
+void printgoodbye();
+
+int main (){
+
+    printhello();
+    printgoodbye();
+    return 0;
+}
+
+void printhello() {
+    printf("Hello User\n");
+}
+
+void printgoodbye(){
+    printf("Good bye user\n");
+}
+// Run On terminial Ctrl + `
+// gcc problem_01.c; .\a.exe (windows)
