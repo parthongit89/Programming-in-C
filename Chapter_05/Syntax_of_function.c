1e4c723acb612002cfc8154b32d351f3caf1406d
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-16 12:32:49 +0530

    New file add of boiler palte of function in C

---
+++ Chapter_05/Syntax_of_function.c
@@ -0,0 +1,19 @@
+// Initicating in function 
+#include <stdio.h>
+
+// Function declaration 
+void printhello(); 
+
+int main(){
+    // Calling Function 
+    printhello();
+    return 0;
+}
+
+// Function block 
+void printhello(){
+    printf("Hello Parth\n");
+}
+
+// Run On terminial Ctrl + `
+// gcc Syntax_of_function.c; .\a.exe (windows)
