d3d7369b667f4a3e2e811672e6002f867947b772
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-17 21:44:13 +0530

    Introduction to pointers in C

---
+++ Chapter_06/Pointers.c
@@ -0,0 +1,12 @@
+// Pointers introduction:
+#include <stdio.h>
+int main(){
+    int n = 25; // Storing value in variable 
+    int *ptr = &n; // location(address) n store in pointer(address store variable) 
+    int _n =*ptr; //accessing those address  from pointer to point value in new variable _n
+    printf("%d",_n);
+    return 0 ;
+}
+
+// Run On terminial Ctrl + `
+// gcc Pointers.c; .\a.exe (windows)
