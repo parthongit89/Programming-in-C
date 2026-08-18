95afaa95cdf9a790b0c090da6252b98ae1c879c5
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-18 21:14:28 +0530

    One line command

--- Chapter_06/Pointers.c
+++ Chapter_06/Pointers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(){
     int n = 25; // Storing value in variable 
-    int *ptr = &n; // location(address) n store in pointer(address store variable) 
+    int *ptr = &n; // location(address) of  n store in pointer(address store variable) 
     int _n =*ptr; //accessing those address  from pointer to point value in new variable _n
     printf("%d",_n);
     return 0 ;
