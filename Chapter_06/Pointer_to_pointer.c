3e73a07deff89e9c5b13ec56c569f9c6569cf7ab
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-18 21:45:33 +0530

    One line command

---
+++ Chapter_06/Pointer_to_pointer.c
@@ -0,0 +1,11 @@
+// Pointer to pointer in c
+#include<stdio.h>
+int main(){
+    int age = 18 ;
+    int *ptr = &age ;
+    int **pptr = &ptr ;
+
+    return 0;
+}
+// Run On terminial Ctrl + `
+// gcc Pointer_to_pointer.c; .\a.exe (windows)
