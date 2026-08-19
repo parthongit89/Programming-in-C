eef4a9e7d56b3c33385db38c0eb6a5c20f4451ca
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-19 22:35:37 +0530

    Introduction of Array in C

---
+++ Chapter_07/Array.c
@@ -0,0 +1,32 @@
+// Boiler plates
+#include<stdio.h>
+int  main() {
+    // Array for integers allocating 2 indexed at a time
+    int _int_array[2];
+
+    // Array for Charcters allocating 10 indexed at a time
+    char _char_arry[10];
+
+    // Array for Float allocating 2 indexed at a time
+    float _float_array[2];
+
+
+    // Input-Output in array
+    int marks[3];
+    printf("Enter the marks 1 : ");
+    scanf("%d",&marks[0]); // &(address)marks(variable-array)[position_memory/index]
+
+    printf("Enter the marks 2 : ");
+    scanf("%d",&marks[1]);
+
+    printf("Enter the marks 3 : ");
+    scanf("%d",&marks[2]);
+    
+    printf("marks_01 : %d, marks_02 : %d , marks_03 : %d",marks[0],marks[1],marks[2]);
+    return 0;
+}
+
+
+// Run On terminial Ctrl + `
+// gcc Array.c; .\a.exe (windows)
+
