2c1ca23aec52db05b563698df7d59f5080857a00
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-22 21:05:29 +0530

    Multidimensional array in C

---
+++ Chapter_07/Multidimensional_array.c
@@ -0,0 +1,15 @@
+// Multidimensional array 2D
+
+# include <stdio.h>
+int main(){
+    int arr[0][1] = {{1,2},{3,4}}; // Declaration
+    return 0;
+
+    // Access
+    arr[0][0]; // --> 1 element
+    arr[0][1]; // --> 2 element
+    arr[1][0]; // --> 3 element
+    arr[1][1]; // --> 4 element
+}
+// Run On terminial Ctrl + `
+// gcc Multidimensional_array.c; .\a.exe (windows)
