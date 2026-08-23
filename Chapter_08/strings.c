d22a9bed2bfebbe703efc4b72d6b2e35f53f64b7
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-23 10:47:35 +0530

    One line command

---
+++ Chapter_08/strings.c
@@ -0,0 +1,17 @@
+//  Introduction of Strings in C
+// A charater array which having \0 null character  -- > termination of strings 
+#include <stdio.h>
+int main(){
+    // Method 1
+    char fs[] = {'P','a','r','t','h','\0'} ;
+    char ls[] = {'S','o','n','a','v','a','n','e','\0'};
+
+    // Method 2
+    char fs_01[] = {"Parth"};
+    char ls_01[] = {"Sonavane"};
+
+    return 0;
+
+}
+// Run On terminial Ctrl + `
+// gcc strings.c; .\a.exe (windows)
