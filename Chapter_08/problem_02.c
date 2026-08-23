d2e250a2e41300b96101099eb3f85400e3d0086a
parthongit89 <parthongit89@users.noreply.github.com>
2026-08-23 11:31:51 +0530

    Use string format specifier in C

--- Chapter_08/problem_02.c
+++ Chapter_08/problem_02.c
@@ -1,10 +1,14 @@
 // Print name of using format specifier in string 
 #include <stdio.h>
 int main(){
-    char name[50];
-    printf("Enter the name :");
-    scanf("%s",name);
-    printf("Your name is  : %s", name);
+    char fs_name[50];
+    char ls_name[50];
+    printf("Enter the first name :");
+    printf("Enter the last name :");
+    scanf("%s",fs_name);
+    scanf("%s",ls_name);
+    printf("Your name is  : %s", fs_name);
+    printf("Your name is  : %s", ls_name);
     return 0;
 }
 // Run On terminial Ctrl + `
