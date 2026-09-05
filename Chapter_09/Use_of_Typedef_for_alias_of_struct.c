061fbcab5a691189930de6b5829793e670fa40c0
parthongit89 <parthongit89@users.noreply.github.com>
2026-09-05 13:42:08 +0530

    Use of typedef in Structres

---
+++ Chapter_09/Use_of_Typedef_for_alias_of_struct.c
@@ -0,0 +1,16 @@
+//Use of typedef for struct for short hand keywords
+#include<stdio.h>
+typedef struct StudentofEngg
+{
+    int id;
+    int bloc;
+}cse;
+
+
+int main(){
+    cse s1; 
+    s1.id  = 261795 ; 
+    s1.bloc = 102 ; 
+
+    return 0;
+}
